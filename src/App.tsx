e11c5f758f44df55d93ef27829183cf813b0ff6f
zhangbx <zhangbx@didiglobal.com>
2020-01-24 00:00:07 +0800

    完成了组件开发，同步流程

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,23 @@ import React, { useState } from 'react';
 import { Layout, Menu, Icon } from 'antd';
 import { Switch, Route, withRouter, NavLink } from 'react-router-dom';
 import NotFound from 'pages/NotFound';
-import { routeCfg } from './config';
+import { routeCfg, IRouteCfgProps } from './config';
 import './App.less';
 
 const { Header, Sider, Content } = Layout;
+const { SubMenu } = Menu;
+
+export function RouteWithSubRoutes(route: IRouteCfgProps) {
+  return (
+    <Route
+      path={route.path}
+      render={props => (
+        // pass the sub-routes down to keep nesting
+        <route.component {...props} routes={route.routes} />
+      )}
+    />
+  );
+}
 
 const App: React.FC = () => {
   const [collapsed, setCollapsed] = useState(false);
@@ -19,19 +32,48 @@ const App: React.FC = () => {
       <Layout>
         <Sider trigger={null} collapsible collapsed={collapsed}>
           <div className="logo" />
-          <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-            <Menu.Item key="/">
-              <NavLink to="/">
-                <Icon type="user" />
-                <span>Home</span>
-              </NavLink>
-            </Menu.Item>
-            <Menu.Item key="/comp">
-              <NavLink to="/comp">
-                <Icon type="video-camera" />
-                <span>组件库</span>
-              </NavLink>
-            </Menu.Item>
+          <Menu
+            theme="dark"
+            mode="inline"
+            defaultSelectedKeys={['home']}
+            defaultOpenKeys={['comp']}
+          >
+            {routeCfg.map(routeItem => {
+              if (routeItem.routes) {
+                return (
+                  <SubMenu
+                    key={routeItem.key}
+                    title={
+                      <span>
+                        {routeItem.icon && <Icon type={routeItem.icon} />}
+                        <span>{routeItem.title}</span>
+                      </span>
+                    }
+                  >
+                    {routeItem.routes.map(subItem => {
+                      return (
+                        <Menu.Item key={subItem.key}>
+                          <NavLink to={subItem.path}>
+                            {subItem.icon && <Icon type={subItem.icon} />}
+                            <span>{subItem.title}</span>
+                          </NavLink>
+                        </Menu.Item>
+                      );
+                    })}
+                  </SubMenu>
+                );
+              } else if (routeItem.component) {
+                return (
+                  <Menu.Item key={routeItem.key}>
+                    <NavLink to={routeItem.path}>
+                      {routeItem.icon && <Icon type={routeItem.icon} />}
+                      <span>{routeItem.title}</span>
+                    </NavLink>
+                  </Menu.Item>
+                );
+              }
+              return null;
+            })}
           </Menu>
         </Sider>
         <Layout>
@@ -44,11 +86,9 @@ const App: React.FC = () => {
           </Header>
           <Content className="App-content">
             <Switch>
-              {routeCfg.map(route => {
-                return (
-                  <Route exact={!!route.exact} component={route.component} path={route.path} />
-                );
-              })}
+              {routeCfg.map(route => (
+                <RouteWithSubRoutes key={route.key} {...route} />
+              ))}
               <Route component={NotFound} />
             </Switch>
           </Content>
