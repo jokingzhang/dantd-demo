1874d37547dbbb985d14b5a7ab08ef7a510d32fb
zhangbx <zhangbx@didiglobal.com>
2020-01-29 18:26:57 +0800

    v1 迁移

---
+++ .docz/app/imports.js
@@ -0,0 +1,22 @@
+export const imports = {
+  'src/components/card/index.mdx': () =>
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "src-components-card-index" */ 'src/components/card/index.mdx'
+    ),
+  'src/components/descriptions/index.mdx': () =>
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "src-components-descriptions-index" */ 'src/components/descriptions/index.mdx'
+    ),
+  'src/components/empty-line/index.mdx': () =>
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "src-components-empty-line-index" */ 'src/components/empty-line/index.mdx'
+    ),
+  'src/components/docs/changeLog.mdx': () =>
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "src-components-docs-change-log" */ 'src/components/docs/changeLog.mdx'
+    ),
+  'src/components/docs/home.mdx': () =>
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "src-components-docs-home" */ 'src/components/docs/home.mdx'
+    ),
+}
---
+++ scripts/moveDeclare.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const klawSync = require('klaw-sync');
+const fs = require('fs');
+
+const declarePaths = klawSync(path.resolve(__dirname, '../dist/src'), {
+  nodir: true,
+});
+
+declarePaths.forEach(pathItem => {
+  const esPath = pathItem.path.replace('/dist/src', '/es');
+  const libPath = pathItem.path.replace('/dist/src', '/lib');
+  fs.copyFileSync(pathItem.path, esPath);
+  fs.copyFileSync(pathItem.path, libPath);
+});
+
+console.log('.d.ts 文件拷贝成功！');
