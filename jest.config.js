620593ae9db83e0dc5afe484c033544ed3a4a36d
zhangbx <zhangbx@didiglobal.com>
2020-01-30 19:30:19 +0800

    增加测试报告

--- jest.config.js
+++ jest.config.js
@@ -15,6 +15,15 @@ module.exports = {
     '<rootDir>/src/**/__tests__/**/*.{ts,tsx,js,jsx,mjs}',
     '<rootDir>/src/**/?(*.)(spec|test).{ts,tsx,js,jsx,mjs}',
   ],
+  reporters: [
+    'default',
+    [
+      './node_modules/jest-html-reporter',
+      {
+        pageTitle: '测试报告',
+      },
+    ],
+  ],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   collectCoverage: true,
   collectCoverageFrom: ['app/react/**/*.{ts,tsx}', '!app/react/__tests__/api/api-test-helpers.ts'],
